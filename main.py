bafab1355d78cb03e7e91484cb0f66ac9fb30572
yukimaru55 <chdyukimaru@gmail.com>
2023-02-08 16:57:19 +0900

    tochuu

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print('aa')
+n = [1,2,3]
+# print (n[0])
+i = int(input("三桁の数字を入力"))
+def validate(ii):
+    if ii < 1000 and ii > 99:
+        return True
+    else :
+        return False
+
+if validate (i):
+    lst = []
+    while i > 0:
+        lst.append(i%10)
+        i //= 10 # 必須
+        lst.reverse()
+    print(lst)
+    if n[0] == lst[0]:
+        if n[1] == lst[1]:
+            if n[2] == lst[2]:
+                print("3イート")
+            elif 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+else:
+    print("ERROR")
+
