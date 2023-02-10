57cfd4e0d75d9ab01c2dbf03ee942000e95876dd
yukimaru55 <chdyukimaru@gmail.com>
2023-02-10 09:44:55 +0900

    ヌメロン完成

--- main.py
+++ main.py
@@ -1,42 +1,44 @@
-n = [1,2,3]
-# print (n[0])
-i = int(input("三桁の数字を入力"))
-def validate(ii):
-    if ii < 1000 and ii > 99:
-        return True
-    else :
-        return False
+import random
+n = [0, 1, 2, 3, 4,5,6,7,8,9]
+n = random.sample(n, 3)
 
-if validate (i):
-    lst = []
-    while i > 0:
-        lst.append(i%10)
-        i //= 10 # 必須
+EATE =0
+while EATE != 3:
+    EATE = 0
+    BITE = 0
+
+    i = int(input("三桁の数字を入力"))
+    def validate(ii):
+        if ii < 1000 and ii > 0:
+            return True
+        else :
+            return False
+
+    
+    if validate (i):
+        lst = []
+        while i > 0:
+            lst.append(i%10)
+            i //= 10 
+        
+      
+        
+        while len(lst) != 3:
+            lst.append(0)
         lst.reverse()
-    print(lst)
-    if n[0] == lst[0]:
-        if n[1] == lst[1]:
-            if n[2] == lst[2]:
-                print("3イート")
-            elif 
+        print(lst)
 
+        
+        for i in range(3):
+            if n[i] == lst[i]:
+                EATE += 1
+            elif lst[i] in n:
+                BITE += 1
+            else:
+                pass
+        print(str(EATE) + ":EATE   " + str(BITE) + ":BITE" )    
+        
+    else:
+        print("ERROR")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-else:
-    print("ERROR")
-
+print('正解です')
